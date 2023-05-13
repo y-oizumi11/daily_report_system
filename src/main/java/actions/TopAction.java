4445d7c7b0ad643d26546081d9a3c4d0bca583e2
yukari.oizumi <y.oizumi95@gmail.com>
2023-05-13 10:10:48 +0900

    トップページとログイン画面表示アクションの作成

---
+++ src/main/java/actions/TopAction.java
@@ -0,0 +1,35 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import constants.AttributeConst;
+import constants.ForwardConst;
+
+
+public class TopAction extends ActionBase {
+
+    @Override
+    public void process() throws ServletException, IOException {
+
+        //メソッドを実行
+        invoke();
+
+    }
+
+
+    public void index() throws ServletException, IOException {
+
+        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //一覧画面を表示
+        forward(ForwardConst.FW_TOP_INDEX);
+    }
+
+}
